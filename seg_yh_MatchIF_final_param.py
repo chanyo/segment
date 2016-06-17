ddb660077f4c3545c7dd8b9382887830b9e34043
Young Hwan Chang <chanyo@ohsu.edu>
2016-06-17 09:34:55 -0700

    Add files via upload

---
+++ seg_yh_MatchIF_final_param.py
@@ -0,0 +1,123 @@
+################################################
+## Young Hwan Chang
+## chanyo@ohsu.edu
+## Do not distribute this CODE
+################################################
+
+
+from __future__ import division
+from optparse import OptionParser
+import numpy as np 
+from skimage import io
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import scale
+import time
+from math import pi
+from skimage.segmentation import mark_boundaries
+import cv2
+
+        
+start_time = time.time()
+
+
+parser = OptionParser()
+parser.add_option("-i", "--input_image", dest="image", type="string", action="store")
+parser.add_option("-o", "--output_directory", dest="output_dir", type="string", action="store")
+parser.add_option("-e", "--extension", dest="ext", type="string", action="store")
+parser.add_option("-g", "--outlines", dest="outlines", type="string", action="store")
+
+
+class Segmentation:
+    def __init__(self, image, output_dir, ext, outlines):
+        self.image_name = str(image)
+        self.output_dir = output_dir
+        self.ext = ext
+        self.outlines = outlines
+        self.I = io.imread(image).astype(np.uint8)
+    
+    def run(self):
+        print "Segmentation Running...."
+        
+        R = self.I[:,:,0]
+        Ilab = cv2.cvtColor(self.I, cv2.COLOR_RGB2LAB)
+        Igray = cv2.cvtColor(Ilab, cv2.COLOR_RGB2GRAY)
+        
+        Ich = Igray
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        Ich = clahe.apply(Igray)  # Or cv2.equalizeHist(Igray)
+        
+        
+        ## Gabor parameter        
+        deg_rads = np.arange(0.0,180.0,45.0)*pi/180.0
+        ksize = 8
+        sigma = 4
+        lambd = 2
+        psi = 0
+        Gfilters = []
+        for theta in deg_rads:
+            kern = cv2.getGaborKernel((ksize, ksize), sigma, theta, lambd, 0,  psi, cv2.CV_64F) 
+            kern /= 1.5*kern.sum()
+            Gfilters.append(kern)
+            
+            
+        ## Gabor Response
+        gabormag = []
+        for kern in Gfilters:
+            accum = np.zeros_like(Ich)
+            fimg = cv2.filter2D(Ich, cv2.CV_8UC3, kern)
+            np.maximum(accum, fimg, accum)
+            gabormag.append(accum)
+            
+        ## Add intensity channel
+        gabormag.append(Ich)
+        
+        
+        ## Define feature
+        X = np.asarray(gabormag)
+        [n_feat, n_x, n_y] = X.shape
+        X_flat = np.reshape(X, (n_feat, n_x*n_y))
+        X_flat = X_flat.astype('float64')
+        Feature = scale(X_flat, axis=1) # Normalization
+        
+        
+        ## Kmean 
+        clusters = 3  # Num of cluster
+        mkb = KMeans(clusters)
+        mkb.fit(np.transpose(Feature))
+        L = mkb.labels_.reshape(n_x, n_y)
+        
+        ## Label     
+        meanI = []
+        for idx in range(clusters):
+            mask_k = R.copy()
+            mask_k[L != idx] = 0
+            meanI.append( np.mean(mask_k[np.nonzero(mask_k)]))
+            
+        id_sort = sorted(range(clusters),key=lambda x:meanI[x], reverse=False)
+        mask = np.zeros_like(R)
+        mask[L == id_sort[0]] = 255
+        
+        
+        ## Final mask (morphological cleaning)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+        mask_final = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        mask_final = cv2.morphologyEx(mask_final, cv2.MORPH_OPEN, kernel)
+    
+
+        ## Save 
+        I_bd = mark_boundaries(self.I, mask_final, color=(0, 1,1))
+        io.imsave(self.outlines+self.ext, I_bd) 
+        io.imsave(self.output_dir+self.ext, mask_final, cmap="gray")   
+        
+        
+        print 'Segmentation done...'
+   
+       
+
+## MAIN   
+(options, args) = parser.parse_args()
+img = Segmentation(options.image, options.output_dir, options.ext, options.outlines)
+img.run()  
+
+
+print time.time() - start_time
